ffeb61ac752d2cedaad7bc49e28e88d6f9010b53
nkwaaaa <nkwaaaa@gmail.com>
2023-03-02 10:56:50 -0300

    add dinamic data to issue items

--- src/issues/views/ListView.tsx
+++ src/issues/views/ListView.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LoadingIcon from "../../shared/components/LoadingIcon";
 import { IssueList } from "../components/IssueList";
 import { LabelPicker } from "../components/LabelPicker";
 import { useIssues } from "../hooks";
@@ -17,7 +18,11 @@ export const ListView = () => {
 	return (
 		<div className="row mt-5">
 			<div className="col-8">
-				<IssueList />
+				{issuesQuery.isLoading ? (
+					<LoadingIcon />
+				) : (
+					<IssueList issues={issuesQuery.data || []} />
+				)}
 			</div>
 
 			<div className="col-4">
